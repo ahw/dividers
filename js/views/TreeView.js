3e90a534416496f2887a747fbe6ce8653e786f49
ahw <andrewhallagan@gmail.com>
2012-10-12 02:32:57 -0400

    Trying to simplify rendering.
    Known bug: redundant "ul" tags.

--- js/views/TreeView.js
+++ js/views/TreeView.js
@@ -1,28 +1,32 @@
 var TreeView = Backbone.View.extend({
 
     el : 'body',
+    addLink : '<a href="#">add</a>',
+    removeLink : '<a href="#">remove</a>',
     
     initialize : function(options) {
         this.model = options.model;
     },
 
     render : function() {
-        var s = sprintf("<ul>%s</ul>", this.renderTree(this.model));
+        var s = this.renderTree(this.model);
+        console.log(s);
         $(this.el).append(s);
         console.log(JSON.stringify(this.model, null, '    '));
     },
 
     renderTree : function(tree) {
         var view = this;
-        var s = sprintf('<li>%s <a href="#">add</a> <a href="#">remove</a>', tree.label);
+        var s = '<ul>\n';
+        s += sprintf('<li>%s %s %s\n', tree.label, view.addLink, view.removeLink)
+
         if (tree.children.length > 0) {
-            s += '<ul>';
             for (var i = 0; i < tree.children.length; i++) {
                 s += view.renderTree(tree.children[i]);
             }
-            s += '</ul>';
         }
-        s += '</li>';
+        s += '</li>\n';
+        s += '</ul>\n';
         return s;
     }
 
