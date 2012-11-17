2a85a42a7428232db4c6e8bf8b9011d5f82d95fa
ahw <andrewhallagan@gmail.com>
2012-11-17 02:49:12 -0500

    Create/Delete operations on EVENTS.
    Can use form and links to create and delete data from EVENTS set. Form
    submission is handled the standard way and server simply redirects back to
    /events. DELETE requests are sent via AJAX and items removed from the page
    using jQuery remove() function.

--- public/js/dividers.js
+++ public/js/dividers.js
@@ -2,21 +2,46 @@ $(document).ready(function() {
 
     var $history= $('#history');
     $('.event').click(function(){ 
-        var ev = $(this).attr('data-ev');
-        var pev = $(this).attr('data-pev');
+        var name = $(this).attr('data-name');
+        var subname = $(this).attr('data-subname');
         var offset = 0;
         $.ajax({
             url : '/history',
-            type : 'post',
+            type : 'POST',
             data : {
-                ev : ev ? ev : "",
-                pev : pev ? pev : "",
+                subname : subname ? subname : "",
+                name : name ? name : "",
                 offset : offset
             },
             success : function(response) {
                 console.log(response);
             }
         });
+    });
+
+    // $('form[method="post"][action="/events"]').submit(function() {
+    //     $.post('/events', $(this).serialize(), function(data) {
+    //         console.log(data);
+    //     });
+    //     return false;
+    // });
+
+    $('.event-delete').click(function() {
+        var deleteLink = $(this);
+        $.ajax({
+            url : '/events',
+            type : 'DELETE',
+            data : {
+                name : $(this).attr('data-name'),
+                subname : deleteLink.attr('data-subname'),
+                fullName : deleteLink.attr('data-fullName')
+            },
+            success : function(response) {
+                deleteLink.prev().remove();
+                deleteLink.remove();
+            }
+        });
+        return false;
     });
 
 });
--- routes/events.js
+++ routes/events.js
@@ -27,5 +27,35 @@ module.exports = function(app) {
 
     });
 
+    app.post('/events', function(req, res) {
 
+        var name = req.body.name;
+        var subname = req.body.subname;
+
+        var fullName = subname ? name + ':' + subname : name;
+        console.log(sprintf('Adding %s to EVENTS', fullName));
+        db.sadd(EVENTS, fullName, function(error, reply) {
+            console.log('error = ' + JSON.stringify(error));
+            console.log('reply = ' + JSON.stringify(reply));
+            res.redirect('/events');
+            // res.json(200, {
+            //     error : error,
+            //     reply : reply
+            // });
+        });
+    });
+
+    app.delete('/events', function(req, res) {
+
+        var fullName = req.body.fullName;
+        db.srem(EVENTS, fullName, function(error, reply) {
+            console.log(sprintf('Removing %s from EVENTS', fullName));
+            console.log('error = ' + JSON.stringify(error));
+            console.log('reply = ' + JSON.stringify(reply));
+            res.json(200, {
+                error : error,
+                reply : reply
+            });
+        });
+    });
 };
