7b20a86ee1019583893394eae35f32367a6bb1f7
ahw <andrewhallagan@gmail.com>
2012-11-19 23:01:47 -0800

    Keeps track of duration of events.
    Changed a number of things. Events should now contain attributes "start",
    "name", and, "duration". Will no longer be identifying events with a
    "name:subname" system, because it's dumb. All events will just follow the
    convention of seperating hierarchical levels with colons. An event named
    "reading:newspaper:nytimes" would be considered a "reading" event, a
    "reading:newspaper" event, and a "reading:newspaper:nytimes" event.
    
    The "duration" attribute of events is calculated at time of insertion. When
    inserting a new event, we can look at the events sitting immediately before
    and after it on the overall timeline of events. Using the start times of
    these events we can update either the just-inserted event's duration, the
    previous event's duration, or both.

--- routes/history.js
+++ routes/history.js
@@ -2,26 +2,31 @@ module.exports = function(app) {
 
     app.get('/history/:format?', function(req, res) {
 
-        console.log('format = ' + req.params.format);
-
         var history = [];
         db.sort(TIMESTAMPS, 'DESC',
             'GET', '#',
             'GET', 'event:*->name',
-            'GET', 'event:*->subname',
+            'GET', 'event:*->start',
+            'GET', 'event:*->duration',
             function(error, reply) {
 
-            for (var i = 0; i < (reply.length / 3); i++) {
-                var timestamp = parseInt(reply[3 * i]);
-                var name      = reply[3 * i + 1];
-                var subname   = reply[3 * i + 2];
-                // Use MomentJS to format the timestamp to human-friendly.
-                var m = moment(timestamp);
+            for (var i = 0; i < (reply.length / 4); i++) {
+                var name = reply[4 * i + 1];
+                var start = parseInt(reply[4 * i + 2]);
+                var duration = parseInt(reply[4 * i + 3]);
+                // Use MomentJS to format the start time to human-friendly.
+                var m = moment(start);
                 var timeFormatted = m.format('ddd MMM Do, h:mm:ss a');
+                var durationString;
+                if (duration) {
+                    durationString = moment.duration(duration, 'milliseconds').humanize(); // Thank you, MomentJS!
+                } else {
+                    durationString = 'Ongoing...';
+                }
                 history.push({
                     name : name,
-                    subname : subname,
-                    timestamp : timestamp,
+                    start : start,
+                    duration : durationString,
                     timeFormatted : timeFormatted,
                     timeDay    : m.format('dddd'),
                     timeMonth  : m.format('MMM'),
@@ -37,11 +42,15 @@ module.exports = function(app) {
 
         var done = function() {
             var context = { history : history };
-            if (req.accepts('html')) {
-                res.render('history', context);
-            } else if (req.accepts('json')) {
-                console.log('Accepts: json');
+            // -- if (req.accepts('html')) {
+            // --     res.render('history', context);
+            // -- } else if (req.accepts('json')) {
+            // --     res.json(history);
+            // -- }
+            if (req.params.format == 'json') {
                 res.json(history);
+            } else {
+                res.render('history', context);
             }
         };
 
@@ -50,13 +59,10 @@ module.exports = function(app) {
 
     app.post('/history', function(req, res) {
         var name = req.body.name;
-        var subname = req.body.subname || "";  // Just to be safe.
         var offset = req.body.offset;
-        var offsetunits = req.body.offsetunits;
         offset = offset ? parseInt(offset) : 0; // Offset defaults to 0.
-        var id = req.body.id;
+        var offsetunits = req.body.offsetunits;
 
-        console.log('offsetunits = ' + offsetunits);
         switch(offsetunits) {
             case 'days':
                 offset = offset * 24;
@@ -68,17 +74,76 @@ module.exports = function(app) {
             default:
                 offset = offset * 1000;
         }
-        console.log('offset = ' + offset);
 
         // Add the timestamp to the TIMESTAMPS sorted set.
-        var now = Date.now(); // Get time in milliseconds.
-        now += offset;
-        db.zadd(TIMESTAMPS, now, now);
-        console.log(sprintf('ZADD %s %s (offset = %s)', now, now, offset));
+        var startTime = Date.now(); // Get time in milliseconds.
+        startTime += offset;
+        db.zadd(TIMESTAMPS, startTime, startTime, function(error, reply) {
+            db.zrank(TIMESTAMPS, startTime, function(error, reply) {
+                var index = reply;
+                db.zrange(TIMESTAMPS, index - 1, index + 1, function(error, reply) {
+                    var hasPrevious = false;
+                    var hasNext = false;
+                    switch(reply.length) {
+                        case 0:
+                            // Assert: Inserting at beginning. There are no
+                            // timestamps either before or after this one.
+                            // It is the first one inserted! We cannot
+                            // calculate its duration.
+                            break;
+                        case 1:
+                            // Should never get here. You'd have to somehow
+                            // have inserted the current timestamp at index
+                            // 1 greater than the size of the TIMESTAMPS
+                            // set.
+                            // TODO: Error log.
+                            break;
+                        case 2:
+                            // Assert: Inserting at very end. If there are
+                            // two timestamps returned, one of them must be
+                            // the current one just inserted.  The other one
+                            // is the one previous.
+                            hasPrevious = true;
+                            break;
+                        case 3:
+                            // Assert: Inserting in the middle. The
+                            // timestamps returned represent the previous,
+                            // current, and next events.
+                            hasPrevious = true;
+                            hasNext = true;
+                            break;
+                        default:
+                            // TODO: Error log.
+                    }
 
-        var eventKey = 'event:' + now;
-        db.hmset(eventKey, 'name', name, 'subname', subname, 'time', now);
-        console.log(sprintf('HSET %s name %s subname %s time %s', eventKey, name, subname, now));
+                    if (hasPrevious) {
+                        var prev = parseInt(reply[0]);
+                        var current = parseInt(reply[1]);
+                        var durationPrevious = current - prev;
+                        db.hmset('event:' + prev, 'duration', durationPrevious);
+                        console.log(sprintf('[Updating a previous event\'s duration]\n\tHMSET %s\n\t\tduration %s', 'event:' + prev, durationPrevious));
+                        db.hmset('event:' + current,
+                            'name', name,
+                            'start', startTime
+                        );
+                        console.log(sprintf('[Inserting a new event without duration]\n\tHMSET %s\n\t\tname %s\n\t\tstart %s', 'event:' + current, name, startTime));
+                    }
+
+                    if (hasNext) {
+                        var current = parseInt(reply[1]);
+                        var next = parseInt(reply[2]);
+                        var durationCurrent = next - current;
+                        db.hmset('event:' + current,
+                            'name', name,
+                            'start', startTime,
+                            'duration', durationCurrent
+                        );
+                        console.log(sprintf('[Inserting a new event with duration]\n\tHMSET %s\n\t\tname %s\n\t\tstart %s\n\t\tduration %s', 'event:' + current, name, startTime, durationCurrent));
+                    }
+                });
+            });
+        });
+
         // Echo the request back.
         res.json(req.body);
     });
