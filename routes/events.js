ec05cb446be3d09ee19fa87682fea83658aa8b59
ahw <andrewhallagan@gmail.com>
2012-11-19 23:16:46 -0800

    Removed instance of "subname".
    Tried to get rid of all references to the old name:subname way of doing
    things. Events are uniquely identified by their "name" attribute only. The
    name attribute will by convention contain colons separating its subcategory
    names.

--- routes/events.js
+++ routes/events.js
@@ -2,41 +2,26 @@ module.exports = function(app) {
 
     app.get('/events', function(req, res) {
 
-        var events = [];
         db.sort(EVENTS, 'ALPHA', function(error, reply) {
-            
+
+            var events = [];
             for (var i = 0; i < reply.length; i++) {
-                events.push({
-                    name : reply[i].split(':')[0],
-                    subname : reply[i].split(':')[1],
-                });
+                events.push({name : reply[i]});
             }
-            done();
 
+            var context = {
+                title : 'dividers',
+                events : events
+            };
+            res.render('index', context);
         });
-
-        var done = function() {
-            if (events.length) {
-                var context = {
-                    title : 'dividers',
-                    events : events
-                };
-                res.render('index', context);
-            }
-        };
-
     });
 
     app.post('/events', function(req, res) {
 
         var name = req.body.name;
-        var subname = req.body.subname;
-
-        var fullName = subname ? name + ':' + subname : name;
-        console.log(sprintf('Adding %s to EVENTS', fullName));
-        db.sadd(EVENTS, fullName, function(error, reply) {
-            console.log('error = ' + JSON.stringify(error));
-            console.log('reply = ' + JSON.stringify(reply));
+        console.log(sprintf('Adding %s to EVENTS', name));
+        db.sadd(EVENTS, name, function(error, reply) {
             res.redirect('/events');
             // res.json(200, {
             //     error : error,
@@ -47,11 +32,9 @@ module.exports = function(app) {
 
     app.delete('/events', function(req, res) {
 
-        var fullName = req.body.fullName;
-        db.srem(EVENTS, fullName, function(error, reply) {
-            console.log(sprintf('Removing %s from EVENTS', fullName));
-            console.log('error = ' + JSON.stringify(error));
-            console.log('reply = ' + JSON.stringify(reply));
+        var name = req.body.name;
+        db.srem(EVENTS, name, function(error, reply) {
+            console.log(sprintf('Removing %s from EVENTS', name));
             res.json(200, {
                 error : error,
                 reply : reply
