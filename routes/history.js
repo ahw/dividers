c50d9f7e6a08568a5483abe26aa1a877cacff4fe
ahw <andrewhallagan@gmail.com>
2012-11-22 00:02:45 -0800

    GET /history/:timestamp implemented.
    Can provide a timestamp as an identifier for an event. In the future, an end
    timestamp can be provided as well in order to get a range.

--- routes/history.js
+++ routes/history.js
@@ -1,59 +1,73 @@
 module.exports = function(app) {
 
-    app.get('/history/:format?', function(req, res) {
+    var getHistory = function(callback, startTime, endTime) {
 
-        var history = [];
-        db.sort(TIMESTAMPS, 'DESC',
-            'GET', '#',
-            'GET', 'event:*->name',
-            'GET', 'event:*->start',
-            'GET', 'event:*->duration',
-            function(error, reply) {
+        var startTime = startTime ? startTime : 0;
+        var endTime = endTime ? endTime : 0;
 
-            for (var i = 0; i < (reply.length / 4); i++) {
-                var name = reply[4 * i + 1];
-                var start = parseInt(reply[4 * i + 2]);
-                var duration = parseInt(reply[4 * i + 3]);
-                // Use MomentJS to format the start time to human-friendly.
-                var m = moment(start);
-                var timeFormatted = m.format('ddd MMM Do, h:mm:ss a');
-                var durationString;
-                if (duration) {
-                    durationString = moment.duration(duration, 'milliseconds').humanize(); // Thank you, MomentJS!
-                } else {
-                    durationString = 'Ongoing...';
+        var multi = db.multi();
+        multi.zrevrank(TIMESTAMPS, endTime);
+        multi.zrevrank(TIMESTAMPS, startTime);
+
+        multi.exec(function(error, replies) {
+            var startIndex = replies[0] ? replies[0] : 0;
+            var limit = replies[1] ? (replies[1] - startIndex) + 1 : -1;
+
+            // TODO Remove
+            console.log('GET /history starting at = ' + startIndex);
+            console.log('GET /history limiting to = ' + limit + ' results');
+
+            var history = [];
+            db.sort(TIMESTAMPS,
+                'LIMIT', startIndex, limit,
+                'DESC',
+                'GET', '#',
+                'GET', 'event:*->name',
+                'GET', 'event:*->start',
+                'GET', 'event:*->duration',
+                function(error, reply) {
+
+                for (var i = 0; i < (reply.length / 4); i++) {
+                    var name = reply[4 * i + 1];
+                    var start = parseInt(reply[4 * i + 2]);
+                    var duration = parseInt(reply[4 * i + 3]);
+                    // Use MomentJS to format the start time to human-friendly.
+                    var m = moment(start);
+                    var timeFormatted = m.format('ddd MMM Do, h:mm:ss a');
+                    var durationString;
+                    if (duration) {
+                        durationString = moment.duration(duration, 'milliseconds').humanize(); // Thank you, MomentJS!
+                    } else {
+                        durationString = 'Ongoing...';
+                    }
+                    history.push({
+                        name : name,
+                        start : start,
+                        durationString : durationString,
+                        duration : duration,
+                        timeFormatted : timeFormatted,
+                        timeDay    : m.format('dddd'),
+                        timeMonth  : m.format('MMM'),
+                        timeDate   : m.format('Do'),
+                        timeHour   : m.format('h'),
+                        timeMinute : m.format('mm'),
+                        timeSecond : m.format('ss'),
+                        timeAmPm   : m.format('a')
+                    });
                 }
-                history.push({
-                    name : name,
-                    start : start,
-                    durationString : durationString,
-                    duration : duration,
-                    timeFormatted : timeFormatted,
-                    timeDay    : m.format('dddd'),
-                    timeMonth  : m.format('MMM'),
-                    timeDate   : m.format('Do'),
-                    timeHour   : m.format('h'),
-                    timeMinute : m.format('mm'),
-                    timeSecond : m.format('ss'),
-                    timeAmPm   : m.format('a')
-                });
-            }
-            done();
+                callback(history);
+            });
         });
+    };
 
-        var done = function() {
+    app.get('/history/:timestamp?', function(req, res) {
+
+        var startTime = req.params.timestamp ? req.params.timestamp : null;
+
+        getHistory(function(history) {
             var context = { history : history };
-            // -- if (req.accepts('html')) {
-            // --     res.render('history', context);
-            // -- } else if (req.accepts('json')) {
-            // --     res.json(history);
-            // -- }
-            if (req.params.format == 'json') {
-                res.json(history);
-            } else {
-                res.render('history', context);
-            }
-        };
+            res.render('history', context);
+        }, startTime, startTime);
 
     });
 
